40cb8bcdcab6244b9e59909ae02d3b908dd564e5
Hannah <hannahbeatrizsampaio@gmail.com>
2025-09-12 11:51:56 -0400

    user.controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,10 +33,14 @@ class UserController extends Controller
 
 
 
-    public function store()
+    public function store(Request $request) 
     {
-
-        dd('store');
+        $input = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        dd($request->validate(  $input));
     }
 
 
