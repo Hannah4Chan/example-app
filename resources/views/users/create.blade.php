d21be839cfe9f5ba0709a884b38d26695e4216d9
Hannah <hannahbeatrizsampaio@gmail.com>
2025-09-12 10:31:02 -0400

    create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.app') 
 
-@section('title', 'Listagem de Usuarios')
+@section('title', 'Cadastro de Usuario')
 
 
 
 @section('content')
 
- <h1 class="title">{{ $greeting }}</h1>
- <img width="250" src="{{ Vite::asset('resources/images/shihtzu.jpg') }}" alt="">
-     @foreach ($users as $user)
-       <div class="user-name">{{ $user->name }} ({{$user->email }})</div> 
-    
-    @endforeach
-@endsection
-
+ <h1>Cadastro de Usuario</h1>
+ 
+ @endsection
+ 
