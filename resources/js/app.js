94cdbae58b4d9f2a7cf9b4ab4355cc0795dfcbdc
Hannah <hannahbeatrizsampaio@gmail.com>
2025-09-16 13:30:31 -0400

    sidebar wrapper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
-import.meta.glob([
-  '../images/**',
-  
-]);
+
 
 console.log('Hello, World Laravel!');
 
@@ -11,3 +8,22 @@ import 'admin-lte';
 
 
 import { OverlayScrollbars, } from 'overlayscrollbars';
+
+ const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+      const Default = {
+        scrollbarTheme: 'os-theme-light',
+        scrollbarAutoHide: 'leave',
+        scrollbarClickScroll: true,
+      };
+      document.addEventListener('DOMContentLoaded', function () {
+        const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+        if (sidebarWrapper && OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined) {
+          OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+            scrollbars: {
+              theme: Default.scrollbarTheme,
+              autoHide: Default.scrollbarAutoHide,
+              clickScroll: Default.scrollbarClickScroll,
+            },
+          });
+        }
+      });
