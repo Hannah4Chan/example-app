c6e59a452dd061bb49b10171753fb94c6fee6d60
Hannah <hannahbeatrizsampaio@gmail.com>
2025-09-17 13:54:16 -0400

    @include

---
+++ resources/views/parts/content-header.blade.php
@@ -0,0 +1,17 @@
+        <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+
+          
+            @hasSection('page-title')
+            @endif
+
+          @yield('content')
+
+
+            <!--begin::Row-->
+
+            <!-- /.row (main row) -->
+          </div>
+          <!--end::Container-->
+        </div>
